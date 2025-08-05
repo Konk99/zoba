2b0aeb40802711294e0ff3bac9ae7f68bf27ff57
Kkonk <konk99@o2.pl>
2025-08-05 19:16:41 +0200

    Add main and news height scalling, add read products from json

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const dialog = document.querySelector('dialog');
 const test = document.querySelector('img');
 
-test.addEventListener('click', () => {
-    dialog.showModal();
-})
+// test.addEventListener('click', () => {
+//     dialog.showModal();
+// })
 
 dialog.addEventListener('click', () => {
     dialog.close();
@@ -14,9 +14,13 @@ const buttons = document.querySelectorAll('.button');
 buttons[0].classList.add('firstOne')
 buttons[1].classList.add('secondOne')
 
-const main = document.querySelector('.main');
-const news = document.querySelector('.news');
+const main = document.getElementById('main');
+const news = document.getElementById('news');
 const body = document.body;
+
+main.style.maxHeight = String(window.innerHeight * 0.8) + 'px'
+news.style.maxHeight = String(window.innerHeight * 0.8) + 'px'
+
 
 if(body.style.width < 450) {
     buttons[0].addEventListener('click', () => {
@@ -42,4 +46,66 @@ if(main.getAttribute('data-active') == 'true' && body.style.width < 450) {
     buttons[1].classList.remove('right');
 }else if(news.getAttribute('data-active') == 'true' && body.style.width < 450) {
     buttons[0].classList.remove('left')
+}
+
+let myData = []
+fetch("./data.json")
+        .then(response => {
+            // console.log(response, response.json(), response.data);
+            return response.json();
+        }).then(function(parsedResponse){
+            console.log(parsedResponse);
+            myData.push(parsedResponse);
+            myData = myData[0];
+            createProducts();
+        })
+
+function createProducts() {
+    const products = document.querySelector(".products");
+    const images = products.querySelector(".images");
+    const productsData = myData.products;
+    
+    productsData.forEach(product => {
+        const productCard = document.createElement('div');
+        const name = document.createElement('h4');    
+        const mainImage = document.createElement('img');
+
+        mainImage.src = "./img/" + product.mainPhoto;
+        mainImage.setAttribute('data-image', 'main')
+        name.innerText = product.name;
+
+        productCard.appendChild(mainImage);
+        productCard.appendChild(name);
+        productCard.id = "productCard";
+
+        images.appendChild(productCard);
+    });
+
+    onImageClickAction(productsData);
+}
+
+function onImageClickAction(productsData) {
+    const productCards = document.querySelectorAll('#productCard')
+    
+    productCards.forEach(card => {
+        const productName = card.querySelector('h4').innerText;
+        const image = card.querySelector('img');
+
+        productsData.forEach(product => {
+            if(product.name == productName) {
+                card.addEventListener('click', () => {
+                    dialog.children[0].replaceChildren();
+                    for(let i = 0; i < product.gallery.length; i++) {
+                        const galleryImagesrc = "./img/" + product.gallery[i]
+                        const newGalleryImage = document.createElement('img');
+                        newGalleryImage.src = galleryImagesrc;
+                        dialog.children[0].append(newGalleryImage);
+                    }
+                    dialog.showModal();
+                })
+            }
+        })
+
+    })
+
 }
